3ffc32b151f4b69f01287a1649cbe06663b8ce00
Ahmad Dolapo Asiyanbola <dolapo@fordsoft.tech>
2024-02-05 05:41:53 +0100

    update

---
+++ src/views/admin/advert/variables/advertData.js
@@ -0,0 +1,23 @@
+export const advertDataColumns = [
+    {
+      Header: "NAME",
+      accessor: "name",
+    },
+    {
+      Header: "PHONE",
+      accessor: "phone",
+    },
+    {
+      Header: "EMAIL",
+      accessor: "email",
+    },
+    {
+      Header: "BUSINESSNAME",
+      accessor: "businessname",
+    },
+    {
+        Header: "CATEGORY",
+        accessor: "category",
+      },
+  ];
+  
