68210cbc5bea1e06bf6845c65f8942068edc1b28
AKANMU MOBOLAJI <johnsonakanmu2017@gmail.com>
2024-03-02 14:20:37 +0100

    latest update as at 3/2/24

---
+++ src/services/ConversatonService.js
@@ -0,0 +1,114 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/v1/actions";
+
+const ConversationService = {
+  async addBlogWithAttachment(blogData, file) {
+    try {
+      const formData = new FormData();
+      formData.append("title", blogData.title);
+      formData.append("content", blogData.content);
+      formData.append("file", file);
+
+      return await axios.post(`${BASE_URL}/blogs`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async addNewAdvertisementWithAttachment(blogData, file) {
+    try {
+      const formData = new FormData();
+      formData.append("title", blogData.title);
+      formData.append("content", blogData.content);
+      formData.append("file", file);
+
+      return await axios.post(`${BASE_URL}/advsertisement`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async addArtisanWithAttachment(artisanData, file) {
+    try {
+      const formData = new FormData();
+      formData.append("name", artisanData.name);
+      formData.append("email", artisanData.content);
+      formData.append("file", file);
+
+      return await axios.post(`${BASE_URL}/artisan`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async deleteBlog(id) {
+    try {
+      return await axios.delete(`${BASE_URL}/deleteBlog/${id}`);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async deleteArtisan(id) {
+    try {
+      return await axios.delete(`${BASE_URL}/deleteArtisan/${id}`);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async deleteAdvertisement(id) {
+    try {
+      return await axios.delete(`${BASE_URL}/deleteAdvertisement/${id}`);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async deleteVisitor(id) {
+    try {
+      return await axios.delete(`${BASE_URL}/deleteVisitor/${id}`);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async updateArtisan(id, payload) {
+    try {
+      return await axios.put(`${BASE_URL}/updateArtisan/${id}`, payload);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async updateBlog(id, payload) {
+    try {
+      return await axios.put(`${BASE_URL}/updateBlog/${id}`, payload);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async updateAdvert(id, payload) {
+    try {
+      return await axios.put(`${BASE_URL}/updateAdvert/${id}`, payload);
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default ConversationService;
---
+++ src/services/EntranceService.js
@@ -0,0 +1,143 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/v1/entrance";
+
+const EntranceService = {
+  async listBlogs(page, limit) {
+    try {
+      return await axios.get(`${BASE_URL}/blogs`, {
+        params: {
+          page,
+          limit,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async listPaginatedAdvertisement(page, limit) {
+    try {
+      return await axios.get(`${BASE_URL}/advsertisements`, {
+        params: {
+          page,
+          limit,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async listPaginatedArtisan(page, limit) {
+    try {
+      return await axios.get(`${BASE_URL}/artisans`, {
+        params: {
+          page,
+          limit,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async rateArtisan(payload) {
+    try {
+      return await axios.post(`${BASE_URL}/artisans/rating`, payload);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async addcomment(payload) {
+    try {
+      return await axios.post(`${BASE_URL}/blogs/comment`, payload);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async listRecentBlogs(page, limit) {
+    try {
+      return await axios.get(`${BASE_URL}/recent/blog`, {
+        params: {
+          page,
+          limit,
+        },
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async activateAccount(payload) {
+    try {
+      return await axios.post(`${BASE_URL}/account/activate`, payload);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async login(payload) {
+    try {
+      return await axios.post(`${BASE_URL}/login`, payload);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async createVisitRequest(visitorRequest) {
+    try {
+      return await axios.post(`${BASE_URL}/visitor/register`, visitorRequest);
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async changedpassword(userId, changePasswordRequest) {
+    try {
+      return await axios.post(
+        `${BASE_URL}/change-password/${userId}`,
+        changePasswordRequest
+      );
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async resetpassword(userId, resetPasswordRequest) {
+    try {
+      return await axios.post(
+        `${BASE_URL}/reset-password/${userId}`,
+        resetPasswordRequest
+      );
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async resetpassword(userId, resetPasswordRequest) {
+    try {
+      return await axios.post(
+        `${BASE_URL}/reset-password/${userId}`,
+        resetPasswordRequest
+      );
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  async forgetpassword(userId, forgotPasswordRequest) {
+    try {
+      return await axios.post(
+        `${BASE_URL}/forgot-password/${userId}`,
+        forgotPasswordRequest
+      );
+    } catch (error) {
+      throw error;
+    }
+  },
+};
+
+export default EntranceService;
