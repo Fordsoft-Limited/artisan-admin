ca276731bec1cd60008273e8e01820ab8d943c0e
odofin-timothy <odofintimothy@gmail.com>
2024-03-17 16:36:33 +0100

    Updated footer

--- src/components/footer/FooterAdmin.js
+++ src/components/footer/FooterAdmin.js
@@ -6,7 +6,6 @@ import {
   List,
   ListItem,
   Text,
-  Button,
   useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
@@ -38,14 +37,14 @@ export default function Footer() {
         {" "}
         &copy; {1900 + new Date().getYear()}
         <Text as='span' fontWeight='500' ms='4px'>
-          Horizon UI. All Rights Reserved. Made with love by
+          Artisan. All Rights Reserved. Made with love by
           <Link
             mx='3px'
             color={textColor}
             href='https://www.simmmple.com?ref=horizon-chakra-free'
             target='_blank'
             fontWeight='700'>
-            Simmmple!
+            Artisan
           </Link>
         </Text>
       </Text>
